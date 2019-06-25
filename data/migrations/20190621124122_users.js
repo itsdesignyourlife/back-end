be7051f474c3c3ca66c73a168945405d1296440b
chrispetito <christopherpetito@gmail.com>
2019-06-25 14:06:16 -0700

    revert timestamp to moment variable

--- data/migrations/20190621124122_users.js
+++ data/migrations/20190621124122_users.js
@@ -23,7 +23,7 @@ exports.up = function(knex, Promise) {
         .onUpdate("CASCADE");
       table.string("postTitle", 128).notNullable();
       table.string("postBody", 128).notNullable();
-      table.timestamps(true, true);
+      table.timestamp('createdAt').defaultTo(date);
       table.integer("engagementScore").notNullable();
       table.integer("energyScore").notNullable();
       //test
